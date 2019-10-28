faef0e978889d42f4b77ef395e8ac8470f480ab2
Lawrence Neal <nealla@lwneal.com>
2019-10-28 11:47:57 -0700

    Initial commit of NMF demo

---
+++ README.md
@@ -0,0 +1,11 @@
+A simple demonstration of non-negative matrix factorization.
+
+Usage:
+
+```
+pip install -r requirements.txt
+python main.py
+```
+
+Requires PyTorch only for the dataset download.
+
---
+++ main.py
@@ -0,0 +1,56 @@
+import time
+import torch
+import imutil
+from torchvision import datasets
+from sklearn.decomposition import NMF
+
+
+# Try playing around with different numbers of components
+n_components = 16
+
+# eg. digit=7 will factorize only sevens
+digit = None
+
+
+def get_mnist(digit=None):
+    data = datasets.mnist.MNIST('./data', download=True)
+    if digit:
+        mnist = data.train_data[data.train_labels == digit]
+    else:
+        mnist = data.train_data
+    mnist = mnist.type(torch.FloatTensor)
+    return mnist
+
+
+def get_filename():
+    return 'output-{}.jpg'.format(int(time.time()*1000))
+
+
+def factorize(data, width=28):
+    nmf = NMF(n_components=n_components, init='random', random_state=0)
+    W = nmf.fit_transform(data.reshape(-1, width*width))
+    H = nmf.components_
+    return H.reshape(-1, width, width)
+
+
+def main():
+    print('Starting NMF demo')
+
+    print('Downloading MNIST data...')
+    mnist = get_mnist(digit=None)
+    filename = get_filename()
+    imutil.show(mnist.mean(0), filename=filename, resize_to=(256,256), caption="Average MNIST digit")
+    print('Output average MNIST digit to {}'.format(filename))
+
+    print('Computing factorization, please wait...')
+    start_time = time.time()
+    clusters = factorize(mnist)
+    print('Completed factorization with {} components in {:.2f}'.format(n_components, time.time() - start_time))
+
+    filename = get_filename()
+    imutil.show(clusters, filename=filename, resize_to=(512,512), caption="NMF Clusters")
+    print('Wrote output file to {}'.format(filename))
+
+
+if __name__ == '__main__':
+    main()
